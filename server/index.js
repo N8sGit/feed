83213a2f4f59d0256b93d4acfaed94354de0b82f
Nathan Anecone <nanecone@gmail.com>
2019-02-21 15:27:39 -0500

    removed db refs

--- server/index.js
+++ server/index.js
@@ -6,12 +6,9 @@ const compression = require('compression')
 const session = require('express-session')
 const passport = require('passport')
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
-const db = require('./db')
-const sessionStore = new SequelizeStore({db})
 const PORT = process.env.PORT || 8080
 const app = express()
 const socketio = require('socket.io')
-const {Post, Category, Image} = require('./db/models')
 const Uploader = require('s3-image-uploader');
 const Twit = require('twit');
 module.exports = app
@@ -27,12 +24,7 @@ let secrets;
 if (process.env.NODE_ENV !== 'production') {
   secrets = require('../secrets')
 }
-// passport registration
-passport.serializeUser((user, done) => done(null, user.id))
-passport.deserializeUser((id, done) =>
-  db.models.user.findById(id)
-    .then(user => done(null, user))
-    .catch(done))
+
 
 const createApp = () => {
   // logging middleware
@@ -48,9 +40,6 @@ const createApp = () => {
   // session middleware with passport
   app.use(session({
     secret: process.env.SESSION_SECRET || 'my best friend is Jon',
-    store: sessionStore,
-    resave: false,
-    saveUninitialized: false
   }))
   app.use(passport.initialize())
   app.use(passport.session())
@@ -119,17 +108,14 @@ const startListening = () => {
   require('./socket')(io)
 }
 
-const syncDb = () => db.sync({})
 
 // This evaluates as true when this file is run directly from the command line,
 // i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
 // It will evaluate false when this module is required by another module - for example,
 // if we wanted to require our app in a test spec
 if (require.main === module) {
-  sessionStore.sync()
-    .then(syncDb)
-    .then(createApp)
-    .then(startListening)
+  createApp()
+  startListening()
 } else {
   createApp()
 }
